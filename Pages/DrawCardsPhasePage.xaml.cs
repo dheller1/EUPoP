99a753f3605d40a2ccc2e04862b25607fd3d1bd5
Dominik Heller <dominik.heller1@gmail.com>
2024-04-22 21:06:29 +0200

    Add project files.

---
+++ Pages/DrawCardsPhasePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace EUPoP.Pages
+{
+	public partial class DrawCardsPhasePage : Page
+	{
+		public DrawCardsPhasePage() {
+			InitializeComponent();
+		}
+	}
+}
